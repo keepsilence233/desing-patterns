403a94ede2115d57344607b873f9d6d3475965bb
leizige <lei.tl@bitsun-inc.com>
2021-07-27 16:22:23 +0800

    add created time

--- strategy/src/main/java/qx/leizige/Application.java
+++ strategy/src/main/java/qx/leizige/Application.java
@@ -3,6 +3,10 @@ package qx.leizige;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author leizige
+ * created 2021/07/27
+ */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
